149b508ebb5ca9d8c119704189e44d8b79d496df
Bruna <bruna@superclient.com.br>
2023-04-24 16:01:46 -0300

    criando store

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,6 @@ export default {
   name: 'app',
   data () {
     return {
-      products: [
-        {name: 'Banana Skin', price: 20},
-        {name: 'Shiny Star', price: 40},
-        {name: 'Green Shells', price: 60},
-        {name: 'Red Shells', price: 80}
-      ]
       
     }
   }
