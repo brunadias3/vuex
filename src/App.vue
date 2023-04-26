dd7682c774ed359fdfc6e71bf9196ffe9f5f08f4
Bruna <bruna@superclient.com.br>
2023-04-26 17:46:48 -0300

    tirando props e passando obj pela store

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <product-list-one v-bind:products="products"></product-list-one>
-    <product-list-two v-bind:products="products"></product-list-two>
-    
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+
   </div>
 </template>
 
@@ -14,21 +14,13 @@ export default {
   components: {
     'product-list-one': ProductListOne,
     'product-list-two': ProductListTwo
-  },
-  name: 'app',
-  data () {
-    return {
-      
-    }
   }
 }
 </script>
 
 <style>
-
 body {
   font-family: Ubuntu;
   color: #555;
 }
-
 </style>
