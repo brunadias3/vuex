23e58d378ac19fe5e3f46ec3c74cb61b8e2b550f
Bruna <bruna@superclient.com.br>
2023-04-27 13:20:23 -0300

    mapping actions & getters

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -12,6 +12,9 @@
 </template>
 
 <script>
+
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
     computed: {
         products() {
@@ -19,12 +22,18 @@ export default {
         },
         saleProducts() {
             return this.$store.getters.saleProducts;
-        }
+        },
+        ...mapGetters([
+            'saleProducts'
+        ])
     },
     methods: {
-        reducePrice: function (amount) {
-            this.$store.dispatch('reducePrice', amount);
-        }
+        // reducePrice: function (amount) {
+        //     this.$store.dispatch('reducePrice', amount);
+        // }
+            ...mapActions([
+                'reducePrice'
+            ])
     }
 }
 </script>
